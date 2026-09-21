f17286a13486c035c1848db31529a12fd61a4ac9
finna-ann <silver.ann6205@gmail.com>
2026-09-21 20:27:02 +0000

    init .env  configs and db setup

---
+++ src/main/java/com/spring/shelfLife/ShelfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.shelfLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfLifeApplication.class, args);
+	}
+
+}
